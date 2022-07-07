7c39164e001245083d7c072f8ffac5c25d1a4279
Marco <m_uno_p@hotmail.com>
2022-07-07 14:27:41 -0500

    Seed the database with airports and flights

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Airports
+Airport.find_or_create_by(code: 'LIM')
+Airport.find_or_create_by(code: 'AQP')
+Airport.find_or_create_by(code: 'CUZ')
+Airport.find_or_create_by(code: 'JUL')
+
+# Flights
+Flight.find_or_create_by(from_airport_id: 1, to_airport_id: 2, scheduled_on: "2022-10-01 15:00", duration: 90)
+Flight.find_or_create_by(from_airport_id: 2, to_airport_id: 1, scheduled_on: "2022-10-10 18:00", duration: 90)
+Flight.find_or_create_by(from_airport_id: 3, to_airport_id: 4, scheduled_on: "2022-11-01 12:00", duration: 50)
+Flight.find_or_create_by(from_airport_id: 4, to_airport_id: 3, scheduled_on: "2022-11-10 15:00", duration: 50)
